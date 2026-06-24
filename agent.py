53d401a980b8337b5a5a6f2465908ec04ce27a64
25G2131 <s25g2131jb@chibatech.ac.jp>
2026-06-24 15:44:07 +0900

    append miyahara-1

--- agent.py
+++ agent.py
@@ -8,6 +8,12 @@ import os
 #パラメータの定義
 WIDTH = 50 #空間の幅
 HEIGHT = 50 #空間の高さ
+
+# 席の右上、左下の座標をタプルで指定、矩形で囲んで領域とする。
+SEAT_START = (,) # 席の領域の右上の座標(x,y)
+SEAT_END = (,)   # 席の領域の左下の座標(x,y)
+SEAT_CENTER = ((SEAT_END[0]-SEAT_START[0])/2,(SEAT_END[1]-SEAT_START[1])/2) # 席の領域の中央点
+
 AGENTS_NUMBER = 300 #エージェントの数
 CAPACITY = 200  #収容人数
 SIMULATION_TIME = 200 #シミュレーション時間
@@ -53,3 +59,12 @@ class Agent:
     def update_direct(self):
 
     def update_stayer(self):
+        # エージェントが"席に行く人"の状態だった場合、席の座標に移動して静止する。
+        if agent.type == TYPE_STAYER:
+            # 席に着くまでずっと移動する。
+            while(not(SEAT_START[0] <= self.x <= SEAT_END[0]) and (SEAT_START[1] <= self.y <= SEAT_END[1])):
+                self = np.random.rand() * 2 * np.pi    # 席のある領域の中央へ向かうように角度を設定
+                self.x += AGENT_SPEED * np.cos(angle)  # x座標更新
+                self.y += AGENT_SPEED * np.sin(angle)  # y座標更新
+                self.x = max(0, min(self.x, WIDTH))    # 席の領域内に収まるように調整
+                self.y = max(0, min(self.y, HEIGHT))   # 席の領域内に収まるように調整
